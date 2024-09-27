fe519bce0c2c8b054d632d85cebd7aea6626eb06
Harjot Singh <121034602+deadlockhook@users.noreply.github.com>
2024-09-27 00:37:03 -0300

    Update game_manager.cs

--- Assets/Managers/game_manager.cs
+++ Assets/Managers/game_manager.cs
@@ -25,13 +25,20 @@ public class game_manager : MonoBehaviour
 
     static bool initialized = false;
 
-    public GlobalGameSaveData _GlobalGameSaveData;
+    public int playerHealth = 0;
+    public int playerShield = 0;
+    public int currentXP = 0;
+    public int currentScore = 0;
+    public int damageModifier = 0;
+    public int scoreModifier = 0;
 
+    static private int gameManagerCount = 0;
     void Awake()
     {
         if (!initialized)
         {
             GameObject.DontDestroyOnLoad(gameObject);
+            gameManagerCount += 1;
             initialized = true;
         }
         else
@@ -42,32 +49,32 @@ public class game_manager : MonoBehaviour
     private void OnGUI()
     {
         GUI.Label(new Rect(10, 10, 200, 30), "currentLevel: " + SceneManager.GetActiveScene().name);
-        GUI.Label(new Rect(10, 40, 200, 30), "currentLevel Index: " + SceneManager.GetActiveScene().buildIndex);
-        GUI.Label(new Rect(10, 70, 200, 30), "playerHealth: " + _GlobalGameSaveData.playerHealth);
-        GUI.Label(new Rect(10, 100, 200, 30), "playerShield: " + _GlobalGameSaveData.playerShield);
-        GUI.Label(new Rect(10, 130, 200, 30), "currentXP: " + _GlobalGameSaveData.currentXP);
-        GUI.Label(new Rect(10, 160, 200, 30), "currentScore: " + _GlobalGameSaveData.currentScore);
-        GUI.Label(new Rect(10, 190, 200, 30), "damageModifier: " + _GlobalGameSaveData.damageModifier);
-        GUI.Label(new Rect(10, 220, 200, 30), "scoreModifier: " + _GlobalGameSaveData.scoreModifier);
-    
+        GUI.Label(new Rect(10, 40, 200, 30), "gameManagerCount: " + gameManagerCount);
+        GUI.Label(new Rect(10, 70, 200, 30), "playerHealth: " + playerHealth);
+        GUI.Label(new Rect(10, 100, 200, 30), "playerShield: " + playerShield);
+        GUI.Label(new Rect(10, 130, 200, 30), "currentXP: " + currentXP);
+        GUI.Label(new Rect(10, 160, 200, 30), "currentScore: " + currentScore);
+        GUI.Label(new Rect(10, 190, 200, 30), "damageModifier: " + damageModifier);
+        GUI.Label(new Rect(10, 220, 200, 30), "scoreModifier: " + scoreModifier);
+
         if (GUI.Button(new Rect(10,300,100,30),"Health up"))
         {
-            _GlobalGameSaveData.playerHealth += 10;
+            playerHealth += 10;
         }
 
         if (GUI.Button(new Rect(10, 340, 100, 30), "Health down"))
         {
-            _GlobalGameSaveData.playerHealth -= 10;
+            playerHealth -= 10;
         }
 
         if (GUI.Button(new Rect(10, 380, 100, 30), "Score up"))
         {
-            _GlobalGameSaveData.currentScore += 10;
+            currentScore += 10;
         }
 
         if (GUI.Button(new Rect(10, 420, 100, 30), "Score down"))
         {
-            _GlobalGameSaveData.currentScore -= 10;
+            currentScore -= 10;
         }
 
         if (GUI.Button(new Rect(10, 460, 100, 30), "Save"))
@@ -86,7 +93,14 @@ public class game_manager : MonoBehaviour
         BinaryFormatter bf = new BinaryFormatter();
         FileStream file = File.Open(Application.persistentDataPath + "/GlobalGameState.dat", FileMode.OpenOrCreate);
         Debug.Log(file.Name);
-        bf.Serialize(file, _GlobalGameSaveData);
+        GlobalGameSaveData saveData = new GlobalGameSaveData();
+        saveData.playerHealth = playerHealth;
+        saveData.playerShield = playerShield;
+        saveData.currentXP = currentXP;
+        saveData.currentScore = currentScore;
+        saveData.damageModifier = damageModifier;
+        saveData.scoreModifier = scoreModifier;
+        bf.Serialize(file, saveData);
         file.Close();
     }
 
@@ -96,7 +110,14 @@ public class game_manager : MonoBehaviour
         {
             BinaryFormatter bf = new BinaryFormatter();
             FileStream file = File.Open(Application.persistentDataPath + "/GlobalGameState.dat", FileMode.Open);
-            _GlobalGameSaveData = (GlobalGameSaveData)bf.Deserialize(file);
+            GlobalGameSaveData saveData = (GlobalGameSaveData)bf.Deserialize(file);
+           playerHealth = saveData.playerHealth;
+            playerShield = saveData.playerShield;
+            currentXP = saveData.currentXP;
+            currentScore = saveData.currentScore;
+            damageModifier = saveData.damageModifier;
+            scoreModifier = saveData.scoreModifier;
+
             file.Close();
         } 
     }
